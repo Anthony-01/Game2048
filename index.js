49c0c6429d917c4551538187d1119ec150fac7a9
Anthony <824283752@qq.com>
2019-09-16 10:38:15 +0800

    eleme1.0

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const app = express();
 const cors = require('cors');
 const path = require("path");
 
-app.use(cors());
+const appData = require('./data.json');
 
-app.use(express.static(path.join(__dirname, '/web')));
+// app.use(cors());
+
+app.use(express.static(path.join(__dirname, '/eleme')));
+
+// app.get("")
+
+app.get('/api/seller', function (req,res, next) {
+    res.json({
+      errno: 0,
+      data: appData.seller
+    })
+  });
+
+  app.get('/api/goods', function(req, res, next) {
+    res.json({
+      errno: 0,
+      data: appData.goods
+    })
+  })
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/web/index.html")
+    res.sendFile(__dirname + "/eleme/index.html")
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
