d7c483aa35ef15f5be4c76a5d5c1cd4b1542eb1c
Anthony <824283752@qq.com>
2019-10-28 11:05:06 +0800

    robot-client

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require("path");
@@ -7,26 +7,24 @@ const appData = require('./data.json');
 
 // app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/eleme')));
+app.use(express.static(path.join(__dirname, '/robot')));
 
-// app.get("")
-
-app.get('/api/seller', function (req,res, next) {
-    res.json({
-      errno: 0,
-      data: appData.seller
-    })
-  });
-
-  app.get('/api/goods', function(req, res, next) {
-    res.json({
-      errno: 0,
-      data: appData.goods
-    })
+app.get('/api/seller', function (req, res, next) {
+  res.json({
+    errno: 0,
+    data: appData.seller
   })
+});
+
+app.get('/api/goods', function(req, res, next) {
+  res.json({
+    errno: 0,
+    data: appData.goods
+  })
+})
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/eleme/index.html")
+    res.sendFile(__dirname + "/robot/index.html");
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
